6e21dd76593110b90a1bc77dab97351c89452334
Sam Ford <samueljamesford@googlemail.com>
2024-06-07 18:01:04 +0100

    Adds logging to backend for deployed bugfix

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -4,17 +4,21 @@ const bcrypt = require("bcrypt");
 
 const AuthenticationController = {
   Authenticate: async (req, res) => {
+    console.log('Attempting Log in!')
     const email = req.body.email;
     const password = req.body.password;
+    console.log('email',email)
+    console.log('password',password)
 
     try {
       const user = await User.findOne({ email: email }).collation({ locale: 'en', strength: 2 });
 
       if (!user) {
+        console.log('No User Found')
         return res.status(401).json({ message: "Email address not recognised" });
       }
+      console.log(user)
       const PasswordMatch = await bcrypt.compare(password, user.password);
-    
       if (!PasswordMatch) {
         return res.status(401).json({ message: "Incorrect password" });
       }
