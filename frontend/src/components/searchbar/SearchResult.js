3787123001aa3afe8beb23e6c4e059ee35f85831
Sam Ford <samueljamesford@googlemail.com>
2024-06-09 21:59:49 +0100

    Adds ProfilePicture to Search Result

--- frontend/src/components/searchbar/SearchResult.js
+++ frontend/src/components/searchbar/SearchResult.js
@@ -8,7 +8,7 @@ export const SearchResult = ({result, id}) => {
     <div className="search-result" key={id}>
         <a href={`/users/${result._id}`}>
             {/* <img src={user.avatar} className='smallcirclemask' alt="Image Alt Text" /> */}
-            <img className='smallcirclemask' src={`https://picsum.photos/seed/${result._id}/300`}/>
+            <img className='smallcirclemask' src={result.avatar} alt='Profile'/>
             <span className='who-liked-this-span'>{`${result.firstName} ${result.lastName}`}</span>
         </a>
     </div>
